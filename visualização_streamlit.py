95ded7da8daf5dd794703779e8a15a4470b8c8cd
RomeroBF <87500697+RomeroBF@users.noreply.github.com>
2025-04-26 13:23:46 -0300

    graficodebarras1

--- visualização_streamlit.py
+++ visualização_streamlit.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plp
 path='machinery_data.csv'
 df=pd.read_csv(path)
+x=df['Machine_Type'].unique()
+y=df.groupby('Machine_Type')['Failures'].sum()
+
+
+
 
 st.title('Dashboard de Produção de Máquinas')
-
+st.bar_chart(x,y)
 
