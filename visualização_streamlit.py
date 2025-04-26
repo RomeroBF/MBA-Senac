b6326378234191ae371bc40060a81a914c61614b
RomeroBF <87500697+RomeroBF@users.noreply.github.com>
2025-04-26 13:41:28 -0300

    Update visualização_streamlit.py

--- visualização_streamlit.py
+++ visualização_streamlit.py
@@ -3,13 +3,9 @@ import streamlit as st
 import matplotlib.pyplot as plp
 path='machinery_data.csv'
 df=pd.read_csv(path)
-x=df['Machine_Type'].unique()
-y=df.groupby('Machine_Type')['Failures'].sum()
-st.dataframe(df.head())
-
-
 
 st.title('Dashboard de Produção de Máquinas')
-st.bar_chart(x,y)
-Qfalhas=df.groupby('Machine_Type')['Failures'].sum()
-st.bar_chart(Qfalhas)
+y=df.groupby('Machine_Type')['Failures'].sum()
+fig, x=plt.subplots()
+x.bar(y['Machine_Type'],y['Failures'])
+st.pyplot(fig)
